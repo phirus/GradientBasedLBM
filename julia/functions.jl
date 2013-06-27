16abbc64e91e21ec76bfc797a2a4857764c480d7
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-06-27 14:46:05 +0200

    optimization works better

--- julia/functions.jl
+++ julia/functions.jl
@@ -1,4 +1,5 @@
 function getOtherParams(g,diameter,sigma)
+	#sigma = 1e-4
 	rho = 		1.0
 	delRho = 	0.5
 	tau = 		0.8
@@ -25,14 +26,14 @@ end
 function transform(x::Vector)
 	x[1] = abs(x[1])
 	x[2] = abs(x[2])
-	x[3] = abs(x[3])
+#	x[3] = 1e-4 * abs(x[3])
 	return x
 end
 
 function targetF(x::Vector)
-	Re_t = 25.0
-	Eo_t = 5.0
-	Mo_t = 1e-4
+	Re_t = 50.0
+	Eo_t = 10.0
+	# Mo_t = 1e-1
 
 	x = transform(x)
 
@@ -40,13 +41,9 @@ function targetF(x::Vector)
 	diameter = 	x[2]
 	sigma = 	x[3]
 
-	# g = 		abs(x[1])
-	# diameter = 	abs(x[2])
-	# sigma = 	abs(x[3])
-
-	Re, Mo, Eo = getOtherParams(g,sigma,diameter)
+	Re, Mo, Eo = getOtherParams(g,diameter,sigma)
 	
-	return (Re_t - Re)^2 + (Eo_t - Eo)^2 + (Mo_t - Mo)^2
+	return (Re_t - Re)^2 + (Eo_t - Eo)^2 
 end
 
 println("functions.jl wurde geladen")
--- julia/unit.jl
+++ julia/unit.jl
@@ -6,26 +6,22 @@ require("functions.jl")
 
 println("given parameters")
 
-g = 9.81
-println("\ng = ",g)
-
-diameter = 0.01
-println("\ndiameter = ",diameter)
-
-sigma = 1e-4
-println("\nsigma = ",sigma)
+g = 10.0
+diameter = 1.0
+sigma = 10000.0
+# delRho = 0.5
 
 
-res = optimize(targetF, [g, diameter, sigma], method = :bfgs, iterations = 10000)
+res = optimize(targetF, [g, diameter, sigma], method = :nelder_mead, iterations = 10000)
 show(res)
 
 x = transform(res.minimum)
 
 g = 		x[1]
 diameter = 	x[2]
-sigma = 	x[3]
+#sigma = 	1e-4 * x[3]
 
-Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau = getOtherParams(g,diameter,sigma)
+Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau = getOtherParams(g,diameter, sigma)
 
 println("\n\n##################")
 println("resulting parameters")
