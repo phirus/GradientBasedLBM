5f9ac3d5c7538ccb252f612a65a3c5ec9bafa404
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-08-06 12:30:22 +0200

    introduced empty preprocess class

---
+++ src/preprocess.h
@@ -0,0 +1,16 @@
+#ifndef PREPROCESS_H
+#define PREPROCESS_H
+
+#include"paramset.h"
+
+using namespace std;
+
+
+/// collection of all parameters used during the simulation
+class Preprocess
+{
+public: 
+    double a;
+};
+
+#endif
