644ed02b7e7d55fee2153df8f3a66f79de1f70e9
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-08-07 13:46:42 +0200

    steup basic elements

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -1,2 +1,13 @@
 #include"preprocess.h"
+Preprocess::Preprocess(double Re, double Mo, double Eo)
+{
+    Reynolds = Re;
+    Morton = Mo;
+    Eotvos = Eo;
+}
 
+const double Preprocess::getTau()const{
+	double tau = (resolution / Mach_max   * sqrt(3) / Reynolds ) + 0.5;
+
+	return tau;
+}
--- src/preprocess.h
+++ src/preprocess.h
@@ -2,6 +2,7 @@
 #define PREPROCESS_H
 
 #include"paramset.h"
+#include"constants.h"
 
 using namespace std;
 
@@ -10,7 +11,28 @@ using namespace std;
 class Preprocess
 {
 public: 
-    double a;
+	// constr
+	Preprocess(double Re, double Mo, double Eo);
+
+
+
+	// get methods
+	const double getReynolds()const{return Reynolds;};
+	const double getMorton()const{return Morton;};
+	const double getEotvos()const{return Eotvos;};
+	const double getResolution()const{return resolution;};
+
+	// set methods
+
+	// calculations
+	const double getTau()const;
+
+private:
+	double Reynolds ; 
+	double Morton;
+	double Eotvos;
+	double resolution; // width of bubble in cells
+
 };
 
 #endif
