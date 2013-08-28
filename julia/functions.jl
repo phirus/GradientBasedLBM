e50ba51d0b99a94cc66ccc2a052c379a4e537071
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-08-28 16:27:25 +0200

    made gamma an optimization parameter

--- julia/functions.jl
+++ julia/functions.jl
@@ -2,29 +2,25 @@ function getTargetReynolds()
 	Re = 50
 end
 
-
 function getTargetEotvos()
-	Eo = 10
+	Eo = 20
 end
 
 function getTargetMorton()
-	Mo = 1e-4
+	Mo = 1e-3
 end
 
 function getTau(Reynolds)
 	tau = (4*sqrt(3)/Reynolds) + 0.5
 end
 
-function getSigma(Eotvos, g, delRho,diameter)
-	sigma = g * delRho * diameter^2 / Eotvos
-end
-
-function getOtherParams(g, diameter)
+function getOtherParams(g, sigma,gamma)
 	rho = 		1.0
-	delRho = 	0.5
-	tau = 	getTau(getTargetReynolds())
-	sigma = getSigma(getTargetEotvos(), g, delRho, diameter)
+	delRho = 	rho * (1- 1/ gamma)
+	diameter = 0.1
 
+	tau = 	getTau(getTargetReynolds())
+	
 	dx = diameter/40
 
 	tmp = 2.14 * sigma / (rho * diameter) + 0.505 *g * diameter
@@ -36,36 +32,36 @@ function getOtherParams(g, diameter)
 	c_s = 10*ut;
 	dt = dx / (sqrt(3)*c_s)
 	nu = c_s^2 * dt *(tau - 0.5)
-	mu = rho * nu;
+	mu = rho * nu
 
-	Re = 4*sqrt(3)/(tau - 0.5)	
+	Re = getTargetReynolds()	
 	Mo = g * mu^4 * delRho/(rho^2 * sigma^3)
 	Eo = (g*delRho*diameter^2)/sigma
 
-	return Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau
+	return Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau, diameter
 end
 
-# function transform(x::Vector)
-# 	x[1] = abs(x[1])
-# 	x[2] = abs(x[2])
-# 	x[3] = abs(x[3])
-# 	return x
-# end
+function transform(x::Vector)
+	x[1] = abs(x[1])
+	x[2] = abs(x[2])
+	x[3] = abs(x[3])
+	return x
+end
 
-# function targetF(x::Vector)
-# 	Eo_t = getTargetEotvos()
-# 	Mo_t = getTargetMorton()
+function targetF(x::Vector)
+	Eo_t = getTargetEotvos()
+	Mo_t = getTargetMorton()
 
-# 	x = transform(x)
+	x = transform(x)
 
-# 	g = 		x[1]
-# 	sigma = 	x[2]
-# 	diameter = 	x[3]
+	g = 		x[1]
+	sigma = 	x[2]
+	gamma = 	x[3]
 	
 
-# 	Re, Mo, Eo = getOtherParams(g, sigma, diameter)
+	Re, Mo, Eo = getOtherParams(g, sigma, gamma)
 	
-# 	return  ((Eo_t - Eo)/Eo_t)^2 + ((Mo_t - Mo)/Mo_t)^2 
-# end
+	return  ((Eo_t - Eo)/Eo_t)^2 + ((Mo_t - Mo)/Mo_t)^2 
+end
 
 println("functions.jl wurde geladen")
--- julia/unit.jl
+++ julia/unit.jl
@@ -2,29 +2,29 @@ using Optim
 
 require("createParamFile.jl")
 require("functions.jl")
-
-
 println("given parameters")
+
+
 
 g = 10.0
 sigma = 1e-4
-diameter = 0.1
+gamma = 2
 
-# res = optimize(targetF, [g, sigma, diameter], method = :nelder_mead, iterations = 10000)
-# show(res)
+res = optimize(targetF, [g, sigma, gamma], method = :nelder_mead, iterations = 10000)
+show(res)
 
-# x = transform(res.minimum)
+x = transform(res.minimum)
 
-# g = 		x[1]
-# sigma = 	x[2]
-# diameter = 	x[3]
+g = 		x[1]
+sigma = 	x[2]
+gamma = 	x[3]
 
-Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau = getOtherParams(g, sigma, diameter)
+Re, Mo, Eo, dx, ut, c_s, dt, nu, mu, rho, delRho, tau, diameter = getOtherParams(g, sigma, gamma)
 
 println("\n\n##################")
 println("resulting parameters")
 
-println("\dx = ",dx)
+println("\ndx = ",dx)
 println("\nterminal rise velocity = ",ut)
 println("\nspeed of sound = ",c_s)
 println("\ntimestep = ",dt)
@@ -33,9 +33,9 @@ println("\nmu = ",mu)
 println("\nrho = ",rho)
 println("\ndelta rho = ",delRho)
 println("\ntau = ",tau)
+println("\ndiameter = ",diameter)
 
 println("\ng = ",g)
-println("\ndiameter = ",diameter)
 println("\nsigma = ",sigma)
 
 println("\nReynolds = ",Re)
