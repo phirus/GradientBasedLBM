a9fecbeb1b220f5831278b02f30d4ab3007f0a34
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-09-03 13:58:38 +0200

    matched the julia create paramFile to the acrual default values

--- julia/createParamFile.jl
+++ julia/createParamFile.jl
@@ -2,11 +2,11 @@ function createPFile(Mo, Eo, c_s, gamma, sigma, g)
 
 	stream = open("parameterFile",true,true,true,true,false)
 	write(stream,"# Parameterset yielding")
-	write(stream,"\n# Reynolds = ")
+	write(stream,"\n Reynolds = ")
 	write(stream, string(REYNOLDS_MAX_INI))
-	write(stream,"\n# Morton = ")
+	write(stream,"\n Morton = ")
 	write(stream, string(Mo))
-	write(stream,"\n# Eotvos = ")
+	write(stream,"\n Eotvos = ")
 	write(stream, string(Eo))
 
 	write(stream, "\n\n# resolution / Cells per Bubble diameter, default [40]")
@@ -17,7 +17,7 @@ function createPFile(Mo, Eo, c_s, gamma, sigma, g)
 	write(stream,"\nrho_l = ")
 	write(stream, string(RHO_L))
 
-	write(stream, "\n\n# density ratio, default [1000]")
+	write(stream, "\n\n# density ratio, default [5]")
 	write(stream,"\ngamma = ")
 	write(stream, string(gamma))
 
@@ -29,7 +29,7 @@ function createPFile(Mo, Eo, c_s, gamma, sigma, g)
 	write(stream,"\ndiameter = ")
 	write(stream, string(DIAMETER))
 
-	write(stream, "\n\n# speed of sound / m s^-1, default [1484]")
+	write(stream, "\n\n# speed of sound / m s^-1, default [10]")
 	write(stream,"\nc_s = ")
 	write(stream, string(c_s))
 
@@ -37,7 +37,7 @@ function createPFile(Mo, Eo, c_s, gamma, sigma, g)
 	write(stream,"\nsigma = ")
 	write(stream, string(sigma))
 
-	write(stream, "\n\n# gravitational constant / m s^-1 default [9.81]")
+	write(stream, "\n\n# gravitational constant / m s^-1 default [10]")
 	write(stream,"\ng = ")
 	write(stream, string(g))
 
