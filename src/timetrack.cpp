c96aa82c56a7398cbb760de24d4df4f183d1769e
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-09-07 18:53:33 +0200

    added empty timetrack class

---
+++ src/timetrack.cpp
@@ -0,0 +1,1 @@
+#include"timetrack.h"
---
+++ src/timetrack.h
@@ -0,0 +1,15 @@
+#ifndef TIMETRACK_H
+#define TIMETRACK_H
+
+using namespace std;
+
+
+/// preprocesses the physical properties to LB properties that are then stored in a ParamSet
+class Timetrack
+{
+public: 
+
+
+};
+
+#endif
