bb540d3daa55a0fff95a866c0dad10409747d28c
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-09-07 19:48:16 +0200

    timetrack class complete, tests are missing

--- src/timetrack.cpp
+++ src/timetrack.cpp
@@ -1,1 +1,16 @@
 #include"timetrack.h"
+
+Timetrack::Timetrack(double iniTime, double fac):
+dtIni(iniTime)
+,factor(fac)
+,count(0)
+,refinelist(1,0)
+{}
+
+double Timetrack::getTime()const{
+	double time(0);	
+	for(unsigned int i = 0; i< refinelist.size(); i++){
+		time += refinelist[i] * pow(factor,i);
+	}
+	return time;
+}
--- src/timetrack.h
+++ src/timetrack.h
@@ -1,15 +1,25 @@
 #ifndef TIMETRACK_H
 #define TIMETRACK_H
 
-using namespace std;
+#include<vector>
+#include<cmath>
 
+using namespace std;
 
 /// preprocesses the physical properties to LB properties that are then stored in a ParamSet
 class Timetrack
 {
 public: 
+	Timetrack(double iniTime = 1e-3, double fac = 1.1);
+	inline void timestep(){count++;};
+	inline void refine(){refinelist.push_back(count);};
+	double getTime()const;
 
-
+private:
+	const double dtIni;
+	const double factor;
+	int count;
+	vector<int> refinelist;
 };
 
 #endif
