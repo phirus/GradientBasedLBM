c5bf54448860ff014718120860e3e1d48c2f02e3
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2013-09-23 13:40:31 +0200

    updated example main

--- example/main.cpp
+++ example/main.cpp
@@ -30,35 +30,31 @@ int main(int argc, char** argv){
     int ymax = 150;
     int xmax = 80;
 
-//    double height = 1; // /m
-//    double dx = height/ymax;
-    double dx = 5e-5;
+    // create a Lattice
 
-    double rho_liquid = 1;
-    double rho_gas= 0.001;
-    double ratio = rho_liquid / rho_gas;
+    Lattice meins(xmax,ymax);
+
+    // ste the parameters
+
+    Preprocess prepro = getFilePreprocess("preprocessFile");
+    ParamSet params = prepro.getParamSet();
+
+    meins.setParams(params);
+
+
+    double rho_liquid = prepro.convertRhoL();
+    double rho_gas = prepro.convertRhoG();
+    // double ratio = rho_liquid / rho_gas;
 
     Cell air(0,rho_gas,false);
     Cell liquid(rho_liquid,0,false);
     Cell wall(0,0,true);
 
-    ParamSet params;
-    params.setAlpha(0.2);
-    params.setRatio(rho_liquid,ratio);
-    params.setSigma(6.74e-15);
-    params.setBeta(0.9);
-
-    params.setDeltaX(dx);
-    params.setSoundSpeed(3);
-//    params.setOmega(1.9597,1.9597,0.1);
-
-    Lattice meins(xmax,ymax);
-    meins.setParams(params);
-
-    int R1 = 20;
+    // setup geometry (bubble at the bottom, x-centered)
+    int R1 = prepro.getResolution()/2;
 
     int xm1 = xmax/2;
-    int ym1 = 2*R1;//-R1;
+    int ym1 = 2*R1;
 
     for(int j=0; j< ymax; j++)
     {
@@ -67,31 +63,6 @@ int main(int argc, char** argv){
             else meins.setCell(i,j,liquid);
         }
     }
-
-
-//    int R2 = R1;
-//
-//    int xm2 = xmax/2;
-//    int ym2 = ymax/2+R1;
-//
-//    for(int j=0; j< ymax; j++)
-//    {
-//        for(int i=0; i< xmax; i++){
-//            if( (i-xm2)*(i-xm2) + (j-ym2)*(j-ym2) < R2*R2 ) meins.setCell(i,j,air);
-//        }
-//    }
-
-//    int height = 40;
-//    int width = 40;
-//
-//    for(int j=0; j< ymax; j++)
-//    {
-//        for(int i=0; i< xmax; i++){
-//            if( i > (xm - width/2) && i < (xm + width/2)   && j > (ym - height/2) && j < (xm + height/2)  ) meins.setCell(j,i,liquid);
-//            else meins.setCell(j,i,air);
-//        }
-//    }
-
 
     meins.bottomWall();
     meins.equilibriumIni();
