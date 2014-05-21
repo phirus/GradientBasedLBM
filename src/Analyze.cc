e6d2619bd73e179c83033fd23b9bb50b48250a9d
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-05-21 11:39:10 +0200

    Get Reynolds from Lattice

--- src/Analyze.cc
+++ src/Analyze.cc
@@ -1,5 +1,32 @@
 #include"Analyze.h"
 
+const double getBubbleVelocity(const Lattice& l)
+{
+    field data = l.getData();
+    ColSet extent = l.getSize();
+
+    VeloSet tmp_velo;
+    ColSet tmp_rho;
+    Vector momentum(0,0);
+    double rho_sum(0);
+
+    for (int x = 0; x<extent[0];x++)
+    {
+        for (int y = 0; y<extent[1];y++)
+        {
+            tmp_velo = data[x][y].getU();
+            tmp_rho = data[x][y].getRho();
+            momentum = momentum + (tmp_velo[1] * tmp_rho[1]);
+            rho_sum += tmp_rho[1];
+        }
+    }
+
+    Vector velocity = momentum * (1.0/ rho_sum);
+
+    double upward_velo = velocity.y;
+    return upward_velo;
+}
+
 const double getEotvos(const ParamSet& params, double resolution)
 {
     const double sigma = params.getSigma();
@@ -23,5 +50,14 @@ const double getReynolds(const ParamSet& params, double velocity, double resolut
 {
     const double tau = 1.0 / params.getOmegaRed();
     const double reynolds = (3 * velocity * resolution) / (tau - 0.5);
+    return reynolds;
+}
+
+const double getReynolds(const Lattice& l, double resolution)
+{
+    const ParamSet params = l.getParams();
+    const double velocity = getBubbleVelocity(l);
+    const double reynolds = getReynolds(params, velocity, resolution);
+
     return reynolds;
 }
--- src/Analyze.h
+++ src/Analyze.h
@@ -7,9 +7,11 @@
 
 using namespace std;
 
+const double getBubbleVelocity(const Lattice& l);
+
 const double getEotvos(const ParamSet& params, double resolution = 40);
 const double getMorton(const ParamSet& params);
 const double getReynolds(const ParamSet& params, double velocity, double resolution = 40);
-const double getBubbleVelocity(Lattice l);
+const double getReynolds(const Lattice& l, double resolution = 40);
 
 #endif
