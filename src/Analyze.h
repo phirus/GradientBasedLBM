170a743694fdae15435b25d3b203a1d1d9effb5f
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-05-21 10:15:26 +0200

    zwischen

--- src/Analyze.h
+++ src/Analyze.h
@@ -10,5 +10,6 @@ using namespace std;
 const double getEotvos(const ParamSet& params, double resolution = 40);
 const double getMorton(const ParamSet& params);
 const double getReynolds(const ParamSet& params, double velocity, double resolution = 40);
+const double getBubbleVelocity(Lattice l);
 
 #endif
