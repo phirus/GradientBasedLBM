d7a8f32c9cd2e154a5f16279f9b17e842d091e9c
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-09-16 12:05:32 +0200

    dumped optimization

--- julia/functions.jl
+++ julia/functions.jl
@@ -2,50 +2,14 @@ function getTau(Re)
 	tau = (RESOLUTION * MACH_MAX * sqrt(3) /  Re) + 0.5
 end
 
-function getOtherParams(g, sigma) 
-
-	gamma = 2;
-	delRho = 	RHO_L * (1- 1/ gamma)
-	tau = 	getTau(REYNOLDS_MAX_INI)
-	dx = 1; #DIAMETER/RESOLUTION
-	# ut = getTerminalRiseVelo(sigma, g)
-	c_s = 1/sqrt(3); #ut / MACH_MAX;
-	dt = 1; # dx / (sqrt(3)*c_s)
-	nu = c_s^2 * dt *(tau - 0.5)
-	mu = RHO_L * nu
-	
-	Mo = g * mu^4 * delRho/(RHO_L^2 * sigma^3)
-	Eo = (g*delRho*RESOLUTION^2)/sigma
-
-	return Mo, Eo, dx, c_s, dt, nu, mu, delRho, tau
-end
-
-function transform(x::Vector) # made a function in case i need another transformation
-	y =  abs(x)
-	return y
-end
-
-function targetF(x::Vector)
-	x = transform(x)
-
-	g = 		x[1]
-	sigma = 	x[2]
-
-	Mo, Eo = getOtherParams(g, sigma)
-	
-	return  ((EOTVOS - Eo)/EOTVOS)^2 + ((MORTON - Mo)/MORTON)^2 
-end
-
-
 function getParams_alter() 
 
 	gamma = 2;
 	delRho = 	RHO_L * (1- 1/ gamma)
 	tau = 	getTau(REYNOLDS_MAX_INI)
-	dx = 1; #DIAMETER/RESOLUTION
-	# ut = getTerminalRiseVelo(sigma, g)
-	c_s = 1/sqrt(3); #ut / MACH_MAX;
-	dt = 1; # dx / (sqrt(3)*c_s)
+	dx = 1; 
+	c_s = 1/sqrt(3); 
+	dt = 1;
 	nu = c_s^2 * dt *(tau - 0.5)
 	mu = RHO_L * nu
 
--- julia/unit.jl
+++ julia/unit.jl
@@ -1,47 +1,36 @@
 using Optim
 
+println("##################\n")
+
 require("createParamFile.jl")
 require("functions.jl")
 require("constants.jl")
 
-# start values
-g = 1e-4 #10.0
-sigma = 1e-4
-gamma = 2
+println("\n##################")
 
-res = optimize(targetF, [g, sigma], method = :nelder_mead, iterations = 10000)
-show(res)
-
-x = transform(res.minimum)
-
-g = 		x[1]
-sigma = 	x[2]
-
-Mo, Eo, dx, c_s, dt, nu, mu, delRho, tau = getOtherParams(g, sigma)
-
-Mo_, Eo_, dx_, c_s_, dt_, nu_, mu_, delRho_, tau_, sigma_, g_ = getParams_alter()
+Mo, Eo, dx, c_s, dt, nu, mu, delRho, tau, sigma, g = getParams_alter()
 
 xi = nu * MU_RATIO
 s_2 = 1/(xi/(c_s^2 * dt) + 1/2)
 
-println("\n\n##################")
+
 println("resulting parameters")
 
-println("\ndx = ",dx,"\t",dx_)
-println("\nspeed of sound = ",c_s,"\t",c_s_)
-println("\ntimestep = ",dt,"\t",dt_)
-println("\nnu = ",nu,"\t",nu_)
-println("\nmu = ",mu,"\t",mu_)
+println("\ndx = ",dx)
+println("\nspeed of sound = ",c_s)
+println("\ntimestep = ",dt)
+println("\nnu = ",nu)
+println("\nmu = ",mu)
 println("\nrho = ",RHO_L)
-println("\ndelta rho = ",delRho,"\t",delRho_)
-println("\ntau = ",tau,"\t",tau_)
+println("\ndelta rho = ",delRho)
+println("\ntau = ",tau)
 println("\ns_2 = ",s_2)
 
-println("\ng = ",g,"\t",g_)
-println("\nsigma = ",sigma,"\t",sigma_)
+println("\ng = ",g)
+println("\nsigma = ",sigma)
 
 println("\nReynolds = ",REYNOLDS_MAX_INI)
-println("\nMorton = ",Mo,"\t",Mo_)
-println("\nEotvos = ",Eo,"\t",Eo_)
+println("\nMorton = ",Mo)
+println("\nEotvos = ",Eo)
 
 createPFile(Mo, Eo, c_s, gamma, sigma, g)
