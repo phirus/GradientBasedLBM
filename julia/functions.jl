c070e5e190bdd9d8201d9aa99466ce670b048f12
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-09-16 11:09:21 +0200

    showed that optimization and straight forward calculation yield thesame result

--- julia/functions.jl
+++ julia/functions.jl
@@ -36,4 +36,27 @@ function targetF(x::Vector)
 	return  ((EOTVOS - Eo)/EOTVOS)^2 + ((MORTON - Mo)/MORTON)^2 
 end
 
+
+function getParams_alter() 
+
+	gamma = 2;
+	delRho = 	RHO_L * (1- 1/ gamma)
+	tau = 	getTau(REYNOLDS_MAX_INI)
+	dx = 1; #DIAMETER/RESOLUTION
+	# ut = getTerminalRiseVelo(sigma, g)
+	c_s = 1/sqrt(3); #ut / MACH_MAX;
+	dt = 1; # dx / (sqrt(3)*c_s)
+	nu = c_s^2 * dt *(tau - 0.5)
+	mu = RHO_L * nu
+
+	sigma = sqrt((EOTVOS * (tau - 0.5)^4) / (81 * RESOLUTION^2 * MORTON)) * RHO_L;
+	g = (EOTVOS * sigma) / ( RHO_L * (1 - 1/gamma) * RESOLUTION^2 ) ;
+	
+	Mo = g * mu^4 * delRho/(RHO_L^2 * sigma^3)
+	Eo = (g*delRho*RESOLUTION^2)/sigma
+
+	return Mo, Eo, dx, c_s, dt, nu, mu, delRho, tau, sigma, g
+end
+
+
 println("functions.jl wurde geladen")
--- julia/unit.jl
+++ julia/unit.jl
@@ -19,27 +19,29 @@ sigma = 	x[2]
 
 Mo, Eo, dx, c_s, dt, nu, mu, delRho, tau = getOtherParams(g, sigma)
 
+Mo_, Eo_, dx_, c_s_, dt_, nu_, mu_, delRho_, tau_, sigma_, g_ = getParams_alter()
+
 xi = nu * MU_RATIO
 s_2 = 1/(xi/(c_s^2 * dt) + 1/2)
 
 println("\n\n##################")
 println("resulting parameters")
 
-println("\ndx = ",dx)
-println("\nspeed of sound = ",c_s)
-println("\ntimestep = ",dt)
-println("\nnu = ",nu)
-println("\nmu = ",mu)
+println("\ndx = ",dx,"\t",dx_)
+println("\nspeed of sound = ",c_s,"\t",c_s_)
+println("\ntimestep = ",dt,"\t",dt_)
+println("\nnu = ",nu,"\t",nu_)
+println("\nmu = ",mu,"\t",mu_)
 println("\nrho = ",RHO_L)
-println("\ndelta rho = ",delRho)
-println("\ntau = ",tau)
+println("\ndelta rho = ",delRho,"\t",delRho_)
+println("\ntau = ",tau,"\t",tau_)
 println("\ns_2 = ",s_2)
 
-println("\ng = ",g)
-println("\nsigma = ",sigma)
+println("\ng = ",g,"\t",g_)
+println("\nsigma = ",sigma,"\t",sigma_)
 
 println("\nReynolds = ",REYNOLDS_MAX_INI)
-println("\nMorton = ",Mo)
-println("\nEotvos = ",Eo)
+println("\nMorton = ",Mo,"\t",Mo_)
+println("\nEotvos = ",Eo,"\t",Eo_)
 
 createPFile(Mo, Eo, c_s, gamma, sigma, g)
