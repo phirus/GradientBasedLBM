d60bbdcb2708c8a1cce98d32aefd7c79f11714e6
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-10-22 10:35:38 +0200

    minor fixes

--- helpers/python/conversion.py
+++ helpers/python/conversion.py
@@ -3,11 +3,11 @@ from paraview.simple import *
 
 print 'Number of files to be converted:', len(sys.argv) - 1
 
-for x in range(1, len(sys.argv)):    
+for x in range(1, len(sys.argv)):        
     inputFile = str(sys.argv[x])
     outputFile = inputFile[:-1] + 'u'
     print x,': Converting ', inputFile, '  ->  ', outputFile
-    r = LegacyVTKReader( FileNames= str(sys.argv[x]) )
+    r = LegacyVTKReader( FileNames= inputFile )
     w = XMLUnstructuredGridWriter()
     w.FileName = outputFile
-    w.UpdatePipeline()
+    w.UpdatePipeline()   
