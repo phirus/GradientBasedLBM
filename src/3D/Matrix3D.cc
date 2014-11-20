7279e22c95331b44f964e367b4a1d27e1d9f85cc
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2014-11-20 12:39:30 +0100

    adapted Matrix3D

--- src/3D/Matrix3D.cc
+++ src/3D/Matrix3D.cc
@@ -32,109 +32,123 @@ Matrix3D::Matrix3D(const boost::multi_array<double,2> &m):matrix(boost::extents[
 }
 
 
-// Matrix2D::Matrix2D(const RelaxationPar &relax, double omega):matrix(boost::extents[9][9])
-// {
-//     for(int i = 0;i<9;i++){
-//         for(int j=0;j<9;j++){
-//             matrix[i][j] = 0;
-//         }
-//     }
-//     matrix[0][0] = 1;
-//     matrix[1][1] = relax.s_2;
-//     matrix[2][2] = relax.s_3;
-//     matrix[3][3] = 1;
-//     matrix[4][4] = relax.s_5;
-//     matrix[5][5] = 1;
-//     matrix[6][6] = relax.s_5;
-//     matrix[7][7] = omega;
-//     matrix[8][8] = omega;
-// }
+Matrix3D::Matrix3D(const RelaxationPar3D &relax, double omega):matrix(boost::extents[19][19])
+{
+    for(int i = 0;i<19;i++){
+        for(int j=0;j<19;j++){
+            matrix[i][j] = 0;
+        }
+    }
+    matrix[0][0] = 1;
+    matrix[1][1] = relax.s_2;
+    matrix[2][2] = relax.s_3;
+    matrix[3][3] = 1;
+    matrix[4][4] = relax.s_5;
+    matrix[5][5] = 1;
+    matrix[6][6] = relax.s_5;
+    matrix[7][7] = 1;
+    matrix[8][8] = relax.s_5;
+
+    matrix[9][9] = omega;
+    matrix[10][10] = relax.s_11;
+    matrix[11][11] = omega;
+    matrix[12][12] = relax.s_11;
+    matrix[13][13] = omega;
+    matrix[14][14] = omega;
+    matrix[15][15] = omega;
+    matrix[16][16] = relax.s_17;
+    matrix[17][17] = relax.s_17;
+    matrix[18][18] = relax.s_17;
+}
 
 Matrix3D::Matrix3D(const Matrix3D &other):matrix(boost::extents[19][19])
 {
     matrix = other.getData();
 }
 
-// //=========================== OPERATORS ===========================
+//=========================== OPERATORS ===========================
 
-// const array2D Matrix2D::operator*(const array2D &other)const {
-//     array2D a;
+const array3D Matrix3D::operator*(const array3D &other)const {
+    array3D a;
 
-//     for (int i= 0; i<9;i++){
-//         double sum = 0;
-//         for(int j = 0; j<9;j++){
-//             sum += other[j] * matrix[i][j];
-//         }
-//         a[i] = sum;
-//     }
-//     return a;
-// }
+    for (int i= 0; i<19;i++){
+        double sum = 0;
+        for(int j = 0; j<19;j++){
+            sum += other[j] * matrix[i][j];
+        }
+        a[i] = sum;
+    }
+    return a;
+}
 
-// const DistributionSetType2D Matrix2D::operator*(const DistributionSetType2D &other)const {
-//     DistributionSetType2D a;
+const DistributionSetType3D Matrix3D::operator*(const DistributionSetType3D &other)const {
+    DistributionSetType3D a;
 
-//     a[0] = *this * other[0];
-//     a[1] = *this * other[1];
+    a[0] = *this * other[0];
+    a[1] = *this * other[1];
  
-//     return a;
-// }
+    return a;
+}
 
-// const Matrix2D Matrix2D::operator*(double other)const{
-//     boost::multi_array<double,2> m(boost::extents[9][9]);
+const Matrix3D Matrix3D::operator*(double other)const{
+    boost::multi_array<double,2> m(boost::extents[19][19]);
 
-//     for(int i = 0;i<9;i++){
-//         for(int j=0;j<9;j++){
-//             m[i][j] = matrix[i][j] * other;
-//         }
-//     }
-//     return Matrix2D(m);
-// }
+    for(int i = 0;i<19;i++){
+        for(int j=0;j<19;j++){
+            m[i][j] = matrix[i][j] * other;
+        }
+    }
+    return Matrix3D(m);
+}
 
-// const Matrix2D Matrix2D::operator+(const Matrix2D &other)const{
-//     boost::multi_array<double,2> m(boost::extents[9][9]); 
-//     boost::multi_array<double,2> mother = other.getData();
+const Matrix3D Matrix3D::operator+(const Matrix3D &other)const{
+    boost::multi_array<double,2> m(boost::extents[19][19]); 
+    boost::multi_array<double,2> mother = other.getData();
 
-//     for(int i = 0;i<9;i++){
-//         for(int j=0;j<9;j++){
-//             m[i][j] = matrix[i][j] + mother[i][j];
-//         }
-//     }
-//     return Matrix2D(m);
-// }
+    for(int i = 0;i<19;i++){
+        for(int j=0;j<19;j++){
+            m[i][j] = matrix[i][j] + mother[i][j];
+        }
+    }
+    return Matrix3D(m);
+}
 
-// const Matrix2D Matrix2D::operator-(const Matrix2D &other)const{
-//     boost::multi_array<double,2> m(boost::extents[9][9]); 
-//     boost::multi_array<double,2> mother = other.getData();
+const Matrix3D Matrix3D::operator-(const Matrix3D &other)const{
+    boost::multi_array<double,2> m(boost::extents[19][19]); 
+    boost::multi_array<double,2> mother = other.getData();
 
-//     for(int i = 0;i<9;i++){
-//         for(int j=0;j<9;j++){
-//             m[i][j] = matrix[i][j] - mother[i][j];
-//         }
-//     }
-//     return Matrix2D(m);
-// }
+    for(int i = 0;i<19;i++){
+        for(int j=0;j<19;j++){
+            m[i][j] = matrix[i][j] - mother[i][j];
+        }
+    }
+    return Matrix3D(m);
+}
 
-// const bool Matrix2D::operator==(const Matrix2D &other)const{
-//     boost::multi_array<double,2> mother = other.getData();
-//     bool equal = false;
-//     if (matrix == mother) equal = true;
+const bool Matrix3D::operator==(const Matrix3D &other)const{
+    boost::multi_array<double,2> mother = other.getData();
+    bool equal = false;
+    if (matrix == mother) equal = true;
 
-//     return equal;
-// }
+    return equal;
+}
 
-// //=========================== OPERATIONS ===========================
+//=========================== OPERATIONS ===========================
 
-// const double Matrix2D::linewise(const array2D &other, int line)const{
-//     double sum = 0;
-//     for(int j = 0; j<9;j++){
-//         sum += other[j] * matrix[line][j];
-//     }
-//     return sum;
-// }
+const double Matrix3D::linewise(const array3D &other, int line)const{
+    double sum = 0;
+    for(int j = 0; j<19;j++){
+        sum += other[j] * matrix[line][j];
+    }
+    return sum;
+}
 
-// //=========================== ACCESSORS ===========================
+//=========================== ACCESSORS ===========================
 
-// void Matrix2D::resetOmega(double omega){
-//     matrix[7][7] = omega;
-//     matrix[8][8] = omega;
-// }
+void Matrix3D::resetOmega(double omega){
+    matrix[9][9]   = omega;
+    matrix[11][11] = omega;
+    matrix[13][13] = omega;
+    matrix[14][14] = omega;
+    matrix[15][15] = omega;
+}
--- src/3D/Matrix3D.h
+++ src/3D/Matrix3D.h
@@ -12,23 +12,23 @@ class Matrix3D
         /// Lifecylce
         Matrix3D(bool identity = false);
         Matrix3D(const boost::multi_array<double,2> &m);
-        // Matrix3D(const RelaxationPar &relax, double omega = 1);
+        Matrix3D(const RelaxationPar3D &relax, double omega = 1);
         Matrix3D(const Matrix3D &other);
 
-        //  /// operators
-        // const array2D operator*(const array2D &other) const;
-        // const DistributionSetType2D operator*(const DistributionSetType2D &other) const;
-        // const Matrix2D operator*(double other)const;
-        // const Matrix2D operator+(const Matrix2D &other)const;
-        // const Matrix2D operator-(const Matrix2D &other)const;
-        // const bool operator==(const Matrix2D &other)const;       
+         /// operators
+        const array3D operator*(const array3D &other) const;
+        const DistributionSetType3D operator*(const DistributionSetType3D &other) const;
+        const Matrix3D operator*(double other)const;
+        const Matrix3D operator+(const Matrix3D &other)const;
+        const Matrix3D operator-(const Matrix3D &other)const;
+        const bool operator==(const Matrix3D &other)const;       
         
-        // /// operations
-        // const double linewise(const array2D &other, int line) const;
+        /// operations
+        const double linewise(const array3D &other, int line) const;
         
         /// accessors
         inline const boost::multi_array<double,2> getData()const{return matrix;};
-        // void resetOmega(double omega);
+        void resetOmega(double omega);
 
     private:
         boost::multi_array<double,2> matrix;
