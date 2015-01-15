02de27d049ef5993a375bb25fae50f81c6b89f83
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2015-01-15 11:14:01 +0100

    added 3D functions to analyze

--- src/Analyze.cc
+++ src/Analyze.cc
@@ -32,6 +32,42 @@ const double getBubbleVelocity(const Lattice2D& l)
     return upward_velo;
 }
 
+const double getBubbleVelocity(const Lattice3D& l)
+{
+    field3D data = l.getData();
+    DimSet3D extent = l.getSize();
+
+    Cell3D tmp_cell;
+    VeloSet3D tmp_velo;
+    ColSet tmp_rho;
+    Vector3D momentum(0,0);
+    double rho_sum(0);
+
+    for (int x = 0; x<extent[0];x++)
+    {
+        for (int y = 0; y<extent[1];y++)
+        {
+            for (int z = 0; z<extent[2];z++)
+            {
+                tmp_cell = data[x][y][z];
+                tmp_cell.calcRho();
+                tmp_velo = tmp_cell.getU();
+                tmp_rho = tmp_cell.getRho();
+                if ( tmp_cell.calcPsi() < -0.99) 
+                {
+                    momentum = momentum + (tmp_velo[1] * tmp_rho[1]);
+                    rho_sum += tmp_rho[1];
+                }
+            }
+         }
+    }
+
+    Vector3D velocity = momentum * (1.0/ rho_sum);
+
+    double upward_velo = velocity.z;
+    return upward_velo;
+}
+
 const double getEotvos(const ParamSet& params, double resolution)
 {
     // const double sigma = params.getSigma();
@@ -78,3 +114,11 @@ const double getReynolds(const Lattice2D& l, double resolution)
     return reynolds;
 }
 
+const double getReynolds(const Lattice3D& l, double resolution)
+{
+    const ParamSet params = l.getParams();
+    const double velocity = getBubbleVelocity(l);
+    const double reynolds = getReynolds(params, velocity, resolution);
+
+    return reynolds;
+}
--- src/Analyze.h
+++ src/Analyze.h
@@ -4,14 +4,17 @@
 #include<cmath>
 
 #include"2D/Lattice2D.h"
+#include"3D/Lattice3D.h"
 
 using namespace std;
 
 const double getBubbleVelocity(const Lattice2D& l);
+const double getBubbleVelocity(const Lattice3D& l);
 
 const double getEotvos(const ParamSet& params, double resolution = 40);
 const double getMorton(const ParamSet& params);
 const double getReynolds(const ParamSet& params, double velocity, double resolution = 40);
 const double getReynolds(const Lattice2D& l, double resolution = 40);
+const double getReynolds(const Lattice3D& l, double resolution = 40);
 
 #endif
