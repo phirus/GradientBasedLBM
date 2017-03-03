b0782b1586ddde7361bfef8e87179f70e4a1e598
Philipp Ruschke <philipp.ruschke@iw.uni-halle.de>
2017-03-03 14:16:56 +0100

    fix file names in merge.jl

--- helpers/julia/merge/merge.jl
+++ helpers/julia/merge/merge.jl
@@ -1,9 +1,9 @@
 #!/usr/bin/env julia
 
-input_file_1 = ARGS[1]
+input_file_1 = "BubblePlot.csv"#ARGS[1]
 data_1 = readdlm(input_file_1, ';')
-input_file_2 = ARGS[2]
+input_file_2 = "Ellipsoid.csv"#ARGS[2]
 data_2 = readdlm(input_file_2, ';')
 
 A = [data_1 data_2[:,2:end] ]
-writedlm("out.csv", A, ';')
+writedlm("BubblePlot_complete.csv", A, ';')
